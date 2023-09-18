4578572a95779b5ee0c38767307d58cfdbab1de6
Nikita Tyrkin <tyrkinn@pm.me>
2023-09-18 23:01:25 +0300

    refactor: rename res to calcResult
    res is not obvious naming for this state variable
    you should always write full name that describes what it holds

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ export type SetState<T> = React.Dispatch<React.SetStateAction<T>>
 
 function App() {
   const [inputLine, setInputLine] = useState<string>("")
-  const [res, setRes] = useState<number>(0)
 
+  const [calcResult, setCalcResult] = useState<number>(0)
 
   return (
     <div className='container mx-auto'>
       <div className='flex justify-center'>
-        <InputLine inputLine={inputLine} res={res} />
-        <CalcButtons setInputLine={setInputLine} inputLine={inputLine} res={res} setRes={setRes} />
+        <InputLine inputLine={inputLine} res={calcResult} />
+        <CalcButtons setInputLine={setInputLine} inputLine={inputLine} res={calcResult} setRes={setCalcResult} />
       </div>
     </div>
   )
